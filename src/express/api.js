1f4072ca4052d6becb58dafa5736bb338786f5f0
Nameer Rizvi <rizvinameer@gmail.com>
2019-11-23 15:05:09 -0500

    Api complete.

--- src/express/api.js
+++ src/express/api.js
@@ -1,5 +1,8 @@
 const shared = require("../shared/index");
-const key = process.env.API_KEY;
+const axios = require("axios");
+
+const app_id = process.env.APP_ID;
+const app_code = process.env.APP_CODE;
 
 module.exports = (req, res) => {
   const { name, maxLength } = shared.input;
@@ -10,6 +13,10 @@ module.exports = (req, res) => {
     : value.length > maxLength
     ? res.status(400).send(`${name} is too long.`)
     : (() => {
-        console.log(value);
+        const url = `https://weather.cit.api.here.com/weather/1.0/report.json?product=forecast_astronomy&name=${value}&app_id=${app_id}&app_code=${app_code}`;
+        axios
+          .get(encodeURI(url))
+          .then(res2 => res.status(200).send(res2.data))
+          .catch(err => res.status(400).send(err));
       })();
 };
