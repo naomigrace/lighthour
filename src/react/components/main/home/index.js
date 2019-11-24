46757a40919d288cf047cf60cc75df39c8eebf70
Nameer Rizvi <rizvinameer@gmail.com>
2019-11-24 00:19:43 -0500

    Get url params.

--- src/react/components/main/home/index.js
+++ src/react/components/main/home/index.js
@@ -2,6 +2,27 @@ import React from "react";
 
 import Form from "./form/index";
 
+import { sanitize, input } from "../../../../shared/index";
+
 export default function HomePage() {
+  var { name } = input;
+  const params = new URLSearchParams(window.location.search);
+  var name = params.get(name);
+  name = sanitize(name);
+  console.log(name);
   return <Form />;
 }
+
+// export function getQuery(q) {
+//   const params = new URLSearchParams(window.location.search);
+//   const get = params.get(q);
+//   const sanitized = sanitize({ val: get });
+//   return sanitized
+//     ? sanitized
+//     : sanitize({ val: window.location.hash })
+//     ? sanitize({ val: window.location.hash })
+//     : null;
+// }
+
+// * decodeURIcomponent not required
+// as decoding is part of sanitized().
