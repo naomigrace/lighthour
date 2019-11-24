8e408866bdaf863b38555a0e4697627f303131fe
Naomi-Grace Panlaqui <panlaqui_naomi-grace@bah.com>
2019-11-24 11:57:33 -0500

    start styling infastructure

--- src/react/components/app.js
+++ src/react/components/app.js
@@ -1,23 +1,30 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router";
-import { ConnectedRouter } from "connected-react-router";
+import React, { Component } from "react"
+import { Route, Switch } from "react-router"
+import { ConnectedRouter } from "connected-react-router"
 
-import { history } from "../redux/store";
+import { history } from "../redux/store"
+import { ThemeProvider } from "styled-components"
+import GlobalStyles from "../styles/GlobalStyles"
 
-import HomePage from "./main/home/index";
-import NotFound from "./main/404";
+import HomePage from "./main/home/index"
+import NotFound from "./main/404"
+import themes from "../styles/themes";
 
 export default class App extends Component {
   render() {
     return (
-      <ConnectedRouter history={history}>
-        <main>
-          <Switch>
-            <Route exact path={"/"} component={HomePage} />
-            <Route component={NotFound} />
-          </Switch>
-        </main>
-      </ConnectedRouter>
+      <ThemeProvider theme={themes[0]}>
+        <GlobalStyles>
+          <ConnectedRouter history={history}>
+            <main>
+              <Switch>
+                <Route exact path={"/"} component={HomePage} />
+                <Route component={NotFound} />
+              </Switch>
+            </main>
+          </ConnectedRouter>
+        </GlobalStyles>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/react/styles/themes.js
@@ -0,0 +1,10 @@
+
+
+const midmorning = {
+    'background-image': 'linear-gradient(180deg, #D1E8FF 37%, #7CDEFF 72%, #79AEF4 100%)',
+    'background-color': '#79AEF4',
+};
+
+export default [
+    midmorning
+]
